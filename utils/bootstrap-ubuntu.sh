7b9f9ed974c199daa16d18be8360a3d128e87fe2
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-12-16 13:33:00 +0800

    [bootstrap/ubuntu] Add sudo

--- utils/bootstrap-ubuntu.sh
+++ utils/bootstrap-ubuntu.sh
@@ -8,7 +8,7 @@ else
 fi
 
 ${SUDO} apt-get update
-${SUDO} apt-get install -y wget apt-transport-https gnupg lsb-release build-essential gfortran curl git
+${SUDO} apt-get install -y wget apt-transport-https gnupg lsb-release build-essential gfortran curl git sudo
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | ${SUDO} tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -sc) main" | ${SUDO} tee /etc/apt/sources.list.d/kitware.list >/dev/null
