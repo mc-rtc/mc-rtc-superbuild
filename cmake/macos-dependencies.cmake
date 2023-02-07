7f08c0d7dd14aed4e9abfc4e1e9fbe0bacbfd3f3
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-02-07 18:52:05 +0900

    [macos] Install numpy and cython via brew

--- cmake/macos-dependencies.cmake
+++ cmake/macos-dependencies.cmake
@@ -21,11 +21,13 @@ set(BREW_DEPENDENCIES
   ninja
   git
   libnotify
+  cython
+  numpy
 )
 if(BUILD_BENCHMARKS)
   list(APPEND BREW_DEPENDENCIES google-benchmark)
 endif()
-set(PIP_DEPENDENCIES Cython coverage nose pytest numpy matplotlib)
+set(PIP_DEPENDENCIES coverage nose pytest matplotlib)
 if(WITH_ROS_SUPPORT AND NOT DEFINED ENV{ROS_DISTRO})
   message(FATAL_ERROR "ROS support is enabled but ROS_DISTRO is not set. Please source the setup before continuing or disable ROS support.")
 endif()
