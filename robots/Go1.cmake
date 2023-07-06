1ccd71f8b95603b15c87df4904a2c31abe0abca5
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-07-06 11:41:29 +0200

    Add Unitree's Go1 robot

---
+++ robots/Go1.cmake
@@ -0,0 +1,17 @@
+option(WITH_GO1 "Build with Unitree's GO1 robot support" OFF)
+
+if(NOT WITH_GO1)
+  return()
+endif()
+
+AddCatkinProject(mc_go1_description
+  GITHUB jrl-umi3218/mc_go1_description
+  GIT_TAG origin/main
+  WORKSPACE data_ws
+)
+
+AddProject(mc_go1
+  GITHUB jrl-umi3218/mc_go1
+  GIT_TAG origin/main
+  DEPENDS mc_go1_description mc_rtc
+)
