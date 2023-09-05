d58717efe6a946474178b9bdd403a75fc8254517
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-09-05 21:08:43 +0900

    Add human_description/mc_human

---
+++ robots/Human.cmake
@@ -0,0 +1,17 @@
+option(WITH_HUMAN "Build human model for mc_rtc" ON)
+
+if(NOT WITH_HUMAN)
+  return()
+endif()
+
+AddCatkinProject(human_description
+  GITHUB jrl-umi3218/human_description
+  GIT_TAG origin/master
+  WORKSPACE data_ws
+)
+
+AddProject(mc_human
+  GITHUB jrl-umi3218/mc_human
+  GIT_TAG origin/master
+  DEPENDS human_description mc_rtc
+)
