a059c210062dba922e87956ca0bdd1db6d1ad218
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-01-16 16:16:42 +0100

    [HRP4] Use new HRP4 model/module repositories

--- robots/HRP4.cmake
+++ robots/HRP4.cmake
@@ -4,14 +4,14 @@ if(NOT WITH_HRP4)
   return()
 endif()
 
-AddCatkinProject(hrp4
-  GITE mc-hrp4/hrp4
-  GIT_TAG origin/master
+AddCatkinProject(hrp4_description
+  GITHUB_PRIVATE isri-aist/hrp4_description
+  GIT_TAG origin/main
   WORKSPACE data_ws
 )
 
 AddProject(mc-hrp4
-  GITE mc-hrp4/mc-hrp4
-  GIT_TAG origin/master
-  DEPENDS hrp4 mc_rtc
+  GITHUB_PRIVATE isri-aist/mc-hrp4
+  GIT_TAG origin/main
+  DEPENDS hrp4_description mc_rtc
 )
