118df441e86d8b50d442d20e02d6e5dcd58702a2
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-01-16 17:23:07 +0100

    [robots] Add HC10DTP robot

---
+++ robots/HC10DTP.cmake
@@ -0,0 +1,17 @@
+option(WITH_HC10DTP "Build Yaskawa HC10DTP support for mc_rtc" OFF)
+
+if(NOT WITH_HC10DTP)
+  return()
+endif()
+
+AddCatkinProject(hc10dtp_description
+  GITE adennaoui/hc10dtp_description
+  GIT_TAG origin/main
+  WORKSPACE data_ws
+)
+
+AddProject(mc_hc10dtp
+  GITE adennaoui/mc_hc10dtp
+  GIT_TAG origin/master
+  DEPENDS hc10dtp_description mc_rtc
+)
