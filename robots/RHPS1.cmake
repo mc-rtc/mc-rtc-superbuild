57e6ff2f05307ff0197abbba86defd33d1a8c9dc
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-03-15 17:36:53 +0100

    [robots] Add RHPS1

---
+++ robots/RHPS1.cmake
@@ -0,0 +1,17 @@
+option(WITH_RHPS1 "Build RHPS1 support" OFF)
+
+if(NOT WITH_RHPS1)
+  return()
+endif()
+
+AddCatkinProject(rhps1_description
+  GITHUB isri-aist/rhps1_description
+  GIT_TAG origin/main
+  WORKSPACE data_ws
+)
+
+AddProject(mc_rhps1
+  GITHUB isri-aist/mc_rhps1
+  GIT_TAG origin/master
+  DEPENDS rhps1_description mc_rtc
+)
