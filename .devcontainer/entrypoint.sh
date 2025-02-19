d082d9efe8decf93115750e4172326401bf404ca
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-02-19 17:21:07 +0100

    [docker] Reduce image size

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,10 @@
+##!/usr/bin/env bash
+
+echo "Welcome to mc-rtc-superbuild image for Ubuntu `lsb_release -cs`!"
+echo "All the tools needed to work with mc_rtc are pre-installed in this image."
+echo "To build, use one of the proposed cmake presets:"
+cd ~/workspace/mc-rtc-superbuild
+cmake --list-presets
+
+export GPG_TTY=$(tty) >> /home/vscode/.bashrc
+exec "$@"
