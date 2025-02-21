cfaca9a819744dce665ac33a2acc6f75486ae59e
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-02-21 12:06:53 +0100

    [devcontainer] Ensure that CCACHE_DIR is correct in all terminals
    Some tools play with XDG_CACHE_HOME variables (see
    https://github.com/amitds1997/remote-nvim.nvim/issues/196) which causes
    a wrong default CCACHE_DIR

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,9 +1,18 @@
 ##!/usr/bin/env bash
 
+echo ""
 echo "Welcome to mc-rtc-superbuild image for Ubuntu `lsb_release -cs`!"
 echo "All the tools needed to work with mc_rtc are pre-installed in this image."
 echo "To build, use one of the proposed cmake presets:"
 cd ~/workspace/mc-rtc-superbuild
 cmake --list-presets
+echo ""
+echo "Please refer to README.md for more information about the superbuild."
+echo ""
 
-exec "$@"
+# Makes GNUPG ask for password in the terminal
+export GPG_TTY=$(tty)
+# Ensure cache paths are the same as during docker build
+# See https://github.com/amitds1997/remote-nvim.nvim/issues/196 
+export CCACHE_DIR=$HOME/.cache/.ccache
+export CYTHON_CACHE_DIR=$HOME/.cache/.cython
