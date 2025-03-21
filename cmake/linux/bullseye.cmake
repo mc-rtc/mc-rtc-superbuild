d5915566d3174358043572fc3506c6067404debe
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-21 19:37:58 +0100

    fix(bullseye) use ros humble

---
+++ cmake/linux/bullseye.cmake
@@ -0,0 +1,42 @@
+set(ROS_DISTRO humble)
+set(ROS_WORKSPACE_INSTALL_PYTHON_DESTINATION "lib/python3/dist-packages")
+set(APT_DEPENDENCIES
+  wget
+  cmake
+  build-essential
+  gfortran
+  doxygen
+  cython
+  cython3
+  python-pip
+  python3-pip
+  python-nose
+  python3-nose
+  python-pytest
+  python3-pytest
+  python-numpy
+  python3-numpy
+  python-coverage
+  python3-coverage
+  python-setuptools
+  python3-setuptools
+  libeigen3-dev
+  doxygen
+  doxygen-latex
+  libboost-all-dev
+  libtinyxml2-dev
+  libgeos++-dev
+  libnanomsg-dev
+  libyaml-cpp-dev
+  libltdl-dev
+  python-git
+  python-pyqt5
+  qt5-default
+  libqwt-qt5-dev
+  python-matplotlib
+  ninja-build
+  libnotify-dev
+)
+if(BUILD_BENCHMARKS)
+  list(APPEND APT_DEPENDENCIES libbenchmark-dev)
+endif()
