4035288155e898870e638c78062c852ad4470ff2
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-05-06 09:14:54 +0200

    ops(devcontainer): Add ubuntu noble

--- utils/bootstrap-linux.sh
+++ utils/bootstrap-linux.sh
@@ -19,7 +19,7 @@ then
 fi
 
 ${SUDO} apt-get update
-${SUDO} apt-get install -y --no-install-recommends wget apt-transport-https software-properties-common gnupg lsb-release build-essential gfortran curl git sudo cmake cmake-curses-gui python3-pip ccache
+${SUDO} apt-get install -y --no-install-recommends wget apt-transport-https software-properties-common gnupg lsb-release build-essential gfortran curl git sudo cmake cmake-curses-gui python3-pip
 if [[ `lsb_release -cs` == "noble" ]]
 then
   ${SUDO} apt-get install -y --no-install-recommends pipx
