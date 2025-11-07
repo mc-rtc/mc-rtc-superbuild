fb48f5f7bb77a448857bb214ca1f0625b1789ca5
ThomasDuvinage <toto.duvinage@gmail.com>
2025-11-07 20:01:38 +0900

    feat(robot): add staubli tx2_60 and tx_60 support

---
+++ robots/Staubli.cmake
@@ -0,0 +1,28 @@
+option(WITH_STAUBLI "Build Staubli support" OFF)
+
+if(WITH_STAUBLI)
+  if(ROS_IS_ROS2)
+    AddCatkinProject(
+      staubli_description
+      GITHUB isri-aist/staubli_description
+      GIT_TAG origin/main
+      WORKSPACE data_ws
+    )
+  else() # ROS1
+    message(ERROR "staubli description is not supported on ROS1.")
+  endif()
+
+  AddProject(
+    mc_staubli
+    GITHUB isri-aist/mc_staubli
+    GIT_TAG origin/main
+    DEPENDS staubli_description mc_rtc
+  )
+
+  AddProject(
+    mc_staubli_val3
+    GITHUB isri-aist/mc_staubli_val3
+    GIT_TAG origin/main
+    DEPENDS mc_staubli
+  )
+endif()
