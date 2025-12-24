129ec4d0509039c1b62bfd63fa236e5ade5ed731
ThomasDuvinage <toto.duvinage@gmail.com>
2025-12-24 13:17:19 +0900

    feat(robot): add Go2 support

---
+++ robots/Go2.cmake
@@ -0,0 +1,19 @@
+option(WITH_GO2 "Build with Unitree's GO2 robot support" OFF)
+
+if(NOT WITH_GO2)
+  return()
+endif()
+
+AddCatkinProject(
+  go2_description
+  GITHUB isri-aist/go2_description
+  GIT_TAG origin/main
+  WORKSPACE data_ws
+)
+
+AddProject(
+  mc_go2
+  GITHUB isri-aist/mc_go2
+  GIT_TAG origin/main
+  DEPENDS go2_description mc_rtc
+)
