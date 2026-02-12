2edb8715f51539ce1cbff2d68f7013890bcfed20
ThomasDuvinage <toto.duvinage@gmail.com>
2026-02-12 11:40:26 +0900

    feat(robot): add mc_kortex to Kinova

--- robots/Kinova.cmake
+++ robots/Kinova.cmake
@@ -21,3 +21,10 @@ AddProject(
   GIT_TAG origin/main
   DEPENDS mc_rtc ros_kortex
 )
+
+AddProject(
+  mc_kortex
+  GITHUB mathieu-celerier/mc_kortex
+  GIT_TAG origin/main
+  DEPENDS mc_rtc ros_kortex
+)
