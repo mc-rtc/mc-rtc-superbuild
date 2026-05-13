35ce712cae9d01a418054f186a4b5896b5b5dcff
ThomasDuvinage <toto.duvinage@gmail.com>
2026-05-13 20:13:01 +0900

    fix(robots): return is not WITH_STAUBLI

--- robots/Staubli.cmake
+++ robots/Staubli.cmake
@@ -1,32 +1,34 @@
 option(WITH_STAUBLI "Build Staubli support" OFF)
 
+if(NOT WITH_STAUBLI)
+  return()
+endif()
+
 if(NOT WITH_ROS_SUPPORT)
   message(FATAL_ERROR "ROS support is required to use the Staubli robot")
 endif()
 
-if(WITH_STAUBLI)
-  if(ROS_IS_ROS2)
-    AddCatkinProject(
-      staubli_description
-      GITHUB isri-aist/staubli_description
-      GIT_TAG origin/main
-      WORKSPACE data_ws
-    )
-  else() # ROS1
-    message(ERROR "staubli description is not supported on ROS1.")
-  endif()
-
-  AddProject(
-    mc_staubli
-    GITHUB isri-aist/mc_staubli
+if(ROS_IS_ROS2)
+  AddCatkinProject(
+    staubli_description
+    GITHUB isri-aist/staubli_description
     GIT_TAG origin/main
-    DEPENDS staubli_description mc_rtc
+    WORKSPACE data_ws
   )
-
-  AddProject(
-    mc_staubli_val3
-    GITHUB isri-aist/mc_staubli_val3
-    GIT_TAG origin/main
-    DEPENDS mc_staubli
-  )
+else() # ROS1
+  message(ERROR "staubli description is not supported on ROS1.")
 endif()
+
+AddProject(
+  mc_staubli
+  GITHUB isri-aist/mc_staubli
+  GIT_TAG origin/main
+  DEPENDS staubli_description mc_rtc
+)
+
+AddProject(
+  mc_staubli_val3
+  GITHUB isri-aist/mc_staubli_val3
+  GIT_TAG origin/main
+  DEPENDS mc_staubli
+)
